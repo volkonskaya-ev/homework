a45d159fb2ac510f2139e63283435ca1ee07fd3e
ev.volkonskaya <ev.rodionova@tensor.ru>
2023-05-09 21:04:16 +0300

    homework lesson 3

---
+++ lesson3/task1.py
@@ -0,0 +1,29 @@
+# Напишите функцию modification(lst), которая принимает список и меняет местами его первый и последний элемент.
+# В исходном списке минимум 2 элемента.
+
+def modification(lst):
+    # Здесь пишем код
+    kolichestvo = len(lst)
+    pervii_element = lst[0]
+    lst[0] = lst[kolichestvo-1]
+    lst[kolichestvo-1] = pervii_element
+    return lst
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+data = [
+    [1, 2, 3],
+    [1, 2, 3, 4, 5],
+    ['н', 'л', 'о', 'с']
+]
+
+test_data = [
+    [3, 2, 1], [5, 2, 3, 4, 1], ['с', 'л', 'о', 'н']
+]
+
+
+for i, d in enumerate(data):
+    assert modification(d) == test_data[i], f'С набором {d} есть ошибка, не проходит проверку'
+    print(f'Тестовый набор {d} прошёл проверку')
+print('Всё ок')
