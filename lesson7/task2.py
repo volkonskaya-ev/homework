3e27db10e894f94e0c77cf835cb376ed19b6e54d
ev.volkonskaya <ev.rodionova@tensor.ru>
2023-05-31 00:25:55 +0300

    homework lesson 7 task 2

---
+++ lesson7/task2.py
@@ -0,0 +1,105 @@
+# Напишите класс PersonInfo
+# Экземпляр класса создается из следующих атрибутов:
+# 1. Строка - "Имя Фамилия"
+# 2. Число - возраст сотрудника
+# 3. Подразделения от головного до того, где работает сотрудник.
+# Реализуйте методы класса:
+# 1. short_name, который возвращает строку Фамилия И.
+# 2. path_deps, возвращает путь "Головное подразделение --> ... --> Конечное подразделение"
+# 3. new_salary, Директор решил проиндексировать зарплаты, и новая зарпалата теперь вычисляет по формуле:
+# 1337*Возраст*суммарное кол-во вхождений трех наиболее часто встречающихся букв из списка подразделений
+# (регистр имеет значение "А" и "а" - разные буквы)
+# Например (Ввод --> Вывод) :
+# PersonInfo('Александр Шленский',
+#            32,
+#            'Разработка', 'УК', 'Автотесты').short_name() --> 'Шленский А.'
+# PersonInfo('Александр Шленский',
+#            32,
+#            'Разработка', 'УК', 'Автотесты').path_deps() -->
+#            'Разработка --> УК --> Автотесты'
+# PersonInfo('Александр Шленский', 32, 'Разработка', 'УК', 'Автотесты').new_salary() --> 385056 т.к.
+# т.к. буква "т" встречается 4 раза, "а" 3 раза, 'о' 2 раза, остальные по одной. Сумма трёх самых частых букв 4+3+2 = 9.
+# 1337*32*9 = 385056
+
+# Здесь пишем код
+class PersonInfo:
+    def __init__(self, name, age, *divisions):
+        self.name = name
+        self.age = age
+        self.divisions = divisions
+
+    def short_name(self):
+        name, surname = self.name.split()
+        return f"{surname} {name[0]}."
+
+    def path_deps(self):
+        k = ""
+        for i in range(len(self.divisions)):
+            k = k + self.divisions[i] + " --> "
+        return k[: -5]
+
+    @staticmethod
+    def repeat_count(our_str):
+        letters_dict = {}
+        for j in range(len(our_str)):
+            count = letters_dict.get(our_str[j])
+            if count is None:
+                count = 0
+            letters_dict[our_str[j]] = count + 1
+        return letters_dict
+
+    def new_salary(self):
+        chars = ""
+        for division in self.divisions:
+            chars = chars + division
+        letters_dict = self.repeat_count(chars)
+        top_chars_count = 0
+        for i in range(3):
+            max_count = 0
+            max_letter = ""
+            for letter in letters_dict.keys():
+                if letters_dict[letter] > max_count:
+                    max_count = letters_dict[letter]
+                    max_letter = letter
+            letters_dict.pop(max_letter)
+            top_chars_count = top_chars_count + max_count
+
+        return 1337 * self.age * top_chars_count
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+first_person = PersonInfo('Александр Шленский', 32, 'Разработка', 'УК', 'Автотесты')
+fourth_person = PersonInfo('Иван Иванов', 26, 'Разработка')
+second_person = PersonInfo('Пётр Валерьев', 47, 'Разработка', 'УК')
+third_person = PersonInfo('Макар Артуров', 51, 'Разработка', 'УК', 'Нефункциональное тестирование', 'Автотестирование')
+
+data = [first_person.short_name,
+        second_person.short_name,
+        third_person.short_name,
+        fourth_person.short_name,
+
+        first_person.path_deps,
+        second_person.path_deps,
+        third_person.path_deps,
+        fourth_person.path_deps,
+
+        first_person.new_salary,
+        second_person.new_salary,
+        third_person.new_salary,
+        fourth_person.new_salary
+        ]
+
+
+test_data = ['Шленский А.', 'Валерьев П.', 'Артуров М.', 'Иванов И.',
+
+             'Разработка --> УК --> Автотесты',
+             'Разработка --> УК',
+             'Разработка --> УК --> Нефункциональное тестирование --> Автотестирование',
+             'Разработка',
+             385056, 314195, 1227366, 173810]
+
+for i, d in enumerate(data):
+    assert_error = f'Не прошла проверка для метода {d.__qualname__} экземпляра с атрибутами {d.__self__.__dict__}'
+    assert d() == test_data[i], assert_error
+    print(f'Набор для метода {d.__qualname__} экземпляра класса с атрибутами {d.__self__.__dict__} прошёл проверку')
+print('Всё ок')
